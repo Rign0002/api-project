3c7143f21879af8a35d12b7fddca74925041429f
Justin <rign0002@algonquinlive.com>
2018-11-12 16:03:08 -0500

    very basic layout for webpage

---
+++ js/main.js
@@ -0,0 +1,6 @@
+/***************************************************************************
+Filename: main.js
+Author: Justin Rignault
+Description: main javascript file linked to index.html for api project
+Date: Nov 12 2018
+***************************************************************************/
